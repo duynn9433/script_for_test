5c2be21851fd0a9670d0cfbbd1e7999d423c602f
duynn9433 <92941533+duynn9433@users.noreply.github.com>
2024-02-26 18:10:40 +0700

    Create install_test_tool.sh

---
+++ install_test_tool.sh
@@ -0,0 +1,32 @@
+#! /bin/bash
+
+# Install Java 17
+echo "# Install Java 17"
+./install_java17.sh
+echo "# Finish install Java 17"
+
+# Jmeter
+filenamezip="apache-jmeter-5.6.2.zip"
+filename="apache-jmeter-5.6.2"
+
+echo "# Install Jmeter"
+echo "Download from GDrive"
+./download_gdrive.sh
+echo "Unzip to user home"
+unzip -o "$filenamezip" -d ~/
+echo "# Finish install Jmeter"
+
+# Install repo epel=release
+echo "# Install epel-release"
+sudo dnf -y install epel-release
+echo "# Finish install epel-release"
+
+# Install htop and nload
+echo "# Install htop and nload"
+sudo dnf -y install htop nload
+echo "# Finish install htop and nload"
+
+# Install Wrk
+echo "# Install Wrk"
+./install_wrk.sh
+echo "# Finish install Wrk"
