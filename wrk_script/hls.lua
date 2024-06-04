b30efbee2126b3f8511ead130043df4c0f6072d4
duynn9433 <92941533+duynn9433@users.noreply.github.com>
2024-06-04 10:01:57 +0700

    Update hls.lua

--- wrk_script/hls.lua
+++ wrk_script/hls.lua
@@ -1,4 +1,5 @@
 local logFile = io.open("debug.log", "a")
+local resultFile = io.open("result.log", "a")
 -- https://es1-p1-netcdn.tv360.vn/netcdn-live/304/output/304-audio_133600_eng=131600-video=6730000.m3u8?timestamp=1817589485&uid=test_12345&token=e543a4f37744f5e4b53f55df86d21a04
 local m3u8Url = "/netcdn-live/304/output/304-audio_133600_eng=131600-video=6730000.m3u8?timestamp=1817589485&uid=test_12345&token=e543a4f37744f5e4b53f55df86d21a04"
 local delay_time_ms = 4000
@@ -8,7 +9,12 @@ function log(message)
     logFile:flush()
 end
 
-log("Script loaded")
+function result_log(message)
+    resultFile:write(message .. "\n")
+    resultFile:flush()
+end
+
+-- log("Script loaded")
 
 function is_valid_url(url)
     local pattern = "^https?://[%w-_%.%?%.:/%+=&]+$"
@@ -55,8 +61,6 @@ function init(args)
 end
  
 function request()
-    -- local r = {}
-    
     -- log("Curr Chunk URL: " .. currChunkUrl)
     if currChunkUrl < lastChunkUrl then 
         currChunkUrl = lastChunkUrl
@@ -85,6 +89,8 @@ response = function(status, headers, body)
         else
             -- log("No valid URL found")
         end
+    elseif status ~= 200 then
+        log(status)
     else
         -- log("Response is not an M3U8 file. Skipping body check.")
     end
@@ -103,15 +109,24 @@ done = function(summary, latency, requests)
     print("TotalTransfer: " .. string.format("%.3f", total_transfer_gb) .. " Gb")
     
     -- Printing the average (mean) latency in milliseconds
-    print("Average: " .. string.format("%.3f", latency.mean / 1000) .. " ms")
+    local avg_latency = latency.mean / 1000
+    print("Average: " .. string.format("%.3f", avg_latency) .. " ms")
     
     -- Printing the maximum latency in milliseconds
-    print("Maximum: " .. string.format("%.3f", latency.max / 1000) .. " ms")
+    local max_latency = latency.max / 1000
+    print("Maximum: " .. string.format("%.3f", max_latency) .. " ms")
       
     -- Printing the standard deviation of latency in milliseconds
-    print("StandardDeviation: " .. string.format("%.3f", latency.stdev / 1000) .. " ms")
+    local std_dev_latency = latency.stdev / 1000
+    print("StandardDeviation: " .. string.format("%.3f", std_dev_latency) .. " ms")
     
     print("Latency Distribution:")
+    local l50 = latency:percentile(50) and latency:percentile(50) / 1000 or 0
+    local l90 = latency:percentile(90) and latency:percentile(90) / 1000 or 0
+    local l95 = latency:percentile(95) and latency:percentile(95) / 1000 or 0
+    local l97 = latency:percentile(97) and latency:percentile(97) / 1000 or 0
+    local l99 = latency:percentile(99) and latency:percentile(99) / 1000 or 0
+    local l9999 = latency:percentile(99.99) and latency:percentile(99.99) / 1000 or 0
     for _, p in pairs({ 50, 90, 95, 97, 99, 99.99 }) do
        local n = latency:percentile(p) / 1000  -- Convert from microseconds to milliseconds
        local msg = "%g%%    %.3f ms"
@@ -120,10 +135,15 @@ done = function(summary, latency, requests)
     
     -- Calculate the percentage of all types of errors
     local connect_errors = summary.errors.connect or 0
+    print("ErrorConnect: " .. connect_errors)
     local read_errors = summary.errors.read or 0
+    print("ErrorRead: " .. read_errors)
     local write_errors = summary.errors.write or 0
+    print("ErrorWrite: " .. write_errors)
     local http_status_errors = summary.errors.status or 0
+    print("ErrorStatus: " .. http_status_errors)
     local timeout_errors = summary.errors.timeout or 0
+    print("ErrorTimeout: " .. timeout_errors)
  
     local total_errors = connect_errors + read_errors + write_errors + http_status_errors + timeout_errors
     local total_request_with_err = total_request + connect_errors + read_errors + write_errors + timeout_errors 	
@@ -143,4 +163,11 @@ done = function(summary, latency, requests)
     -- Calculate transfer per second in GB/s
     local tps_gb = total_bytes * 8 / (duration_seconds * 1024 * 1024 * 1024) -- converting bytes to gigabytes
     print("TransferPerSecond: " .. string.format("%.3f", tps_gb) .. " Gb/s")
+
+    --print to csv 
+    result_log("TotalRequests,TotalTime,TotalTransfer,Average,Maximum,StandardDeviation,P50,P90,P95,P97,P99,P99.99,ErrorConnect,ErrorRead,ErrorWrite,ErrorStatus,ErrorTimeout,ErrorPercentage,RequestsPerSecond,TransferPerSecond,TransferPerSecondRAW,")
+    result_log(string.format("%d,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%.3f,%d,%d,%d,%d,%d,%.3f,%.3f,%.3f,%.3f,\n",
+    total_request, time, total_transfer_gb, avg_latency, max_latency, std_dev_latency,
+    l50, l90, l95, l97, l99, l9999,
+    connect_errors, read_errors, write_errors, http_status_errors, timeout_errors, error_percentage, rps, tps_gb, tps))
  end
